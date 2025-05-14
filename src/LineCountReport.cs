34eb2d8ddc49ee1a9a8fec940fde08402befab15
dragon7307 <tim.clemens0703@gmail.com>
2025-05-14 16:34:28 +0200

    cleaned up LineCountReport

--- src/LineCountReport.cs
+++ src/LineCountReport.cs
@@ -1,24 +1,9 @@
 ﻿namespace LineCount;
 
-public record LineCountReport(int Lines, int Files)
+public record LineCountReport(int Lines, int Files = 1)
 {
-    public static LineCountReport FromLines(int lines)
-    {
-        return new LineCountReport(lines, 1);
-    }
-
-    public static implicit operator (int lines, int files)(LineCountReport value)
-    {
-        return (value.Lines, value.Files);
-    }
-
-    public static implicit operator LineCountReport((int lines, int files) value)
-    {
-        return new LineCountReport(value.lines, value.files);
-    }
-
     public static LineCountReport operator +(LineCountReport left, LineCountReport right)
     {
-        return (left.Lines + right.Lines, left.Files + right.Files);
+        return new LineCountReport(left.Lines + right.Lines, left.Files + right.Files);
     }
 }
