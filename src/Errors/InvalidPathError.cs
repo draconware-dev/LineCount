515b93400312bf08ac9ef66481033a43a2e9902f
dragon7307 <tim.clemens0703@gmail.com>
2025-05-14 16:03:52 +0200

    improved some diagnostics

--- src/Errors/InvalidPathError.cs
+++ src/Errors/InvalidPathError.cs
@@ -5,7 +5,7 @@ public sealed record InvalidPathError(string Path) : Error
     public override string ToString()
     {
         var invalidChars = System.IO.Path.GetInvalidPathChars();
-        return $"'{Path}' is invalid. {GetMessage(invalidChars)}";
+        return $"'{Path}' is invalid. {GetMessage(invalidChars)} or exceed {short.MaxValue} characters in length.";
     }
 
     static string GetMessage<T>(T[] characters)
@@ -13,12 +13,12 @@ public sealed record InvalidPathError(string Path) : Error
         switch (characters.Length)
         {
             case 1:
-                return $"It must not contain '{characters[0]}'.";
+                return $"It must not contain '{characters[0]}'";
             case 2:
-                return $"It must contain neither '{characters[0]}' nor '{characters[1]}'.";
+                return $"It must contain neither '{characters[0]}' nor '{characters[1]}'";
             default:
                 string optionsText = string.Join(", ", characters[..^1].Select(x => $"'{x}'"));
-                return $"It must not contain any of {optionsText} or '{characters[^1]}'.";
+                return $"It must not contain any of {optionsText} or '{characters[^1]}'";
         }
     }
 }
---
+++ src/Errors/UndiagnosedError.cs
@@ -0,0 +1,6 @@
+﻿namespace LineCount.Errors;
+
+public sealed record UndiagnosedError(Exception Exception) : ReportError(Exception.Message)
+{
+    protected override string Name => "Something unexpectedly went wrong";
+}
