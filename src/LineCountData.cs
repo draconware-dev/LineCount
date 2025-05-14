cffcf7095c4d3a101a1238539d781f839287ae38
dragon7307 <tim.clemens0703@gmail.com>
2025-05-14 17:28:48 +0200

    added timeout to regex

--- src/LineCountData.cs
+++ src/LineCountData.cs
@@ -7,17 +7,19 @@ public class LineCountData
 {
     public required string? Filter { get; init; }
     public required Regex? LineFilter { get; init; }
-    public required string? FilterNot { get; init; }
-    public required Regex? LineFilterNot { get; init; }
+    public required string? ExcludeFilter { get; init; }
+    public required Regex? ExcludeLineFilter { get; init; }
 
     public FilterType FilterType { get; } = (FilterType)(-1);
     public required bool ListFiles { get; init; }
+
+    public static readonly TimeSpan TimeOut = TimeSpan.FromMilliseconds(500);
 
     [SetsRequiredMembers]
     public LineCountData(string? filter, string? lineFilter, string? filterNot, string? lineFilterNot, bool listFiles)
     {
         Filter = filter;
-        FilterNot = filterNot;
+        ExcludeFilter = filterNot;
         ListFiles = listFiles;
 
         if (lineFilter is null)
@@ -26,13 +28,13 @@ public class LineCountData
         }
         else
         {
-            LineFilter = new Regex(lineFilter, RegexOptions.Singleline | RegexOptions.Compiled);
+            LineFilter = new Regex(lineFilter, RegexOptions.Singleline | RegexOptions.Compiled, TimeOut);
             FilterType = lineFilterNot is null ? FilterType.Filtered : FilterType.FilteredBoth;
         }
 
         if (lineFilterNot is not null)
         {
-            LineFilterNot = new Regex(lineFilterNot, RegexOptions.Singleline | RegexOptions.Compiled);
+            ExcludeLineFilter = new Regex(lineFilterNot, RegexOptions.Singleline | RegexOptions.Compiled, TimeOut);
         }
 
         ListFiles = listFiles;
