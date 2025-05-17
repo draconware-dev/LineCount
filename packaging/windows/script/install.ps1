22f651dcadcd808463248ad73cbe8570b9638eda
dragon7307 <tim.clemens0703@gmail.com>
2025-05-17 10:59:16 +0200

    coped with windows and powershell quirks and improved install.ps1

--- packaging/windows/script/install.ps1
+++ packaging/windows/script/install.ps1
@@ -8,18 +8,41 @@ param(
 $InstallationPath = $InstallationPath.TrimEnd('/', '\')
 New-Item -ItemType Directory -Path $InstallationPath -Force
 
-$response = Invoke-WebRequest $Source
-$path = $response.BaseResponse.ResponseUri.AbsolutePath
+$lastUrlIndex = $Source.LastIndexOfAny([char[]]@('/','\'))
+$fileName = $Source.Substring($lastUrlIndex + 1)
+Write-Output "Downloading $fileName..." 
 
-Expand-Archive -Path $path -DestinationPath $InstallationPath -Force
+Invoke-WebRequest $Source -OutFile $InstallationPath/archive.zip
 
-$root = $Scope == "User" ? "HKCU:" : "HKLM:"
+Write-Output "Extracting $fileName..."
 
-New-Item -Path "$root\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\linecount.exe" -Force | Out-Null
-Set-ItemProperty -Path "$root\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\linecount.exe" -Name "(Default)" -Value "$InstallationPath\linecount.exe"
+Expand-Archive -Path $InstallationPath/archive.zip -DestinationPath $InstallationPath -Force  | Out-Null
+Remove-Item -Path $InstallationPath/archive.zip -Force
+
+Write-Output "Adding linecount to PATH..."
+
+$environment = ($Scope -eq "User") ? "Environment" : "SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
+
+$registry = ($Scope -eq "User") ? [Microsoft.Win32.Registry]::CurrentUser : [Microsoft.Win32.Registry]::LocalMachine
+$key = $registry.OpenSubKey("$environment", $true)
+$currentPATH = $key.GetValue('Path', '', [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames)
+$currentPATH = "$currentPATH;$InstallationPath"
+$key.SetValue('Path', $currentPATH, [Microsoft.Win32.RegistryValueKind]::ExpandString)
+$key.Close()
+
+Write-Output "Generating uninstall.ps1..."
 
 New-Item -Type File -Name "uninstall.ps1" -Path $InstallationPath -Force -Value @"
-Remove-Item -Path "$root\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\linecount.exe" -Force | Out-Null
-Remove-Item -Path "$InstallationPath\linecount.exe" -Force | Out-Null
-Remove-Item -Path "$InstallationPath\linecount.exe" -Force | Out-Null
+`$registry = ("$Scope" -eq "User") ? [Microsoft.Win32.Registry]::CurrentUser : [Microsoft.Win32.Registry]::LocalMachine
+`$key = `$registry.OpenSubKey("$environment", `$true)
+`$currentPATH = `$key.GetValue('Path', '', [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames)
+`$escaped = [Regex]::Escape("$InstallationPath")
+`$currentPATH = ([regex]";?`$escaped").Replace("`$currentPATH", "", 1)
+`$key.SetValue('Path', `$currentPATH, [Microsoft.Win32.RegistryValueKind]::ExpandString)
+`$key.Close()
+Remove-Item -Path "$InstallationPath\linecount.exe" -Force
+Write-Output "Uninstallation complete."
+Remove-Item -Path "$InstallationPath\uninstall.ps1" -Force
 "@ | Out-Null
+
+Write-Output "`nInstallation complete."
