f915b67f777993e3dc08fb743266a108ef2c9377
dragon7307 <tim.clemens0703@gmail.com>
2025-05-24 15:09:42 +0200

    fixed filecount when filtering by line

--- src/LineCountReport.cs
+++ src/LineCountReport.cs
@@ -1,9 +1,14 @@
 ﻿namespace LineCount;
 
-public record LineCountReport(int Lines, int Files = 1)
+public record LineCountReport(int Lines, int Files)
 {
     public static LineCountReport operator +(LineCountReport left, LineCountReport right)
     {
         return new LineCountReport(left.Lines + right.Lines, left.Files + right.Files);
+    }
+
+    public static LineCountReport FromLines(int lines)
+    {
+        return new LineCountReport(lines, lines > 0 ? 1 : 0);
     }
 }
