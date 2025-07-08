dfd3beafd2a652a653719e73c3f6f008861aff61
dragon7307 <tim.clemens0703@gmail.com>
2025-07-08 20:47:06 +0200

    refined filtering

---
+++ src/Globbing.cs
@@ -0,0 +1,36 @@
+﻿using SpanExtensions;
+
+namespace LineCount;
+
+public static class Globbing
+{
+    public static string ToRegex(string pattern)
+    {
+        if (!pattern.All(IsValidGlobPatternChar))
+        {
+            return pattern;
+        }
+
+        if (pattern.StartsWith('*') && pattern.EndsWith('*'))
+        {
+            return pattern[1..^1];
+        }
+
+        if (pattern.StartsWith('*'))
+        {
+            return $"{pattern[1..]}$";
+        }
+
+        if (pattern.EndsWith('*'))
+        {
+            return $"^{pattern[..^1]}";
+        }
+
+        return $"^{pattern}$";
+    }
+
+    static bool IsValidGlobPatternChar(char c)
+    {
+        return c == '*' || c == '_' || c == '-' || c == '.' || char.IsLetterOrDigit(c);
+    }
+}
--- src/LineCountData.cs
+++ src/LineCountData.cs
@@ -1,13 +1,14 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 
 namespace LineCount;
 
 public class LineCountData
 {
-    public string? Filter { get; }
+    public Regex? Filter { get; }
     public Regex? LineFilter { get; }
-    public string? ExcludeFilter { get; }
+    public Regex? ExcludeFilter { get; }
     public Regex? ExcludeLineFilter { get; }
 
     public FilterType FilterType { get; } = (FilterType)(-1);
@@ -17,17 +18,26 @@ public class LineCountData
 
     public LineCountData(string? filter, string? lineFilter, string? filterNot, string? lineFilterNot)
     {
-        Filter = filter;
-        ExcludeFilter = filterNot;
-        
+        if(filter is not null)
+        {
+            string filterPattern = Globbing.ToRegex(filter);
+            Filter = new Regex(filterPattern, RegexOptions.Singleline | RegexOptions.Compiled, TimeOut);
+        }
+
+        if (filterNot is not null)
+        {
+            string filterNotPattern = Globbing.ToRegex(filterNot);
+            ExcludeFilter = new Regex(filterNotPattern, RegexOptions.Singleline | RegexOptions.Compiled, TimeOut);
+        }
+
         if (lineFilter is null)
         {
             FilterType = lineFilterNot is null ? FilterType.None : FilterType.FilteredExcept;
         }
         else
         {
-            LineFilter = new Regex(lineFilter, RegexOptions.Singleline | RegexOptions.Compiled, TimeOut);
             FilterType = lineFilterNot is null ? FilterType.Filtered : FilterType.FilteredBoth;
+            LineFilter = new Regex(lineFilter, RegexOptions.Singleline | RegexOptions.Compiled, TimeOut);
         }
 
         if (lineFilterNot is not null)
